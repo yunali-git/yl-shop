aa15d3148181b2dd1f03deb6b54285deccc226be
yuanli-git <y_licom@163.com>
2020-08-27 21:48:45 +0800

    搭建框架,查询数据

---
+++ yl-shop-parent/yl-shop-commoms/yl-shop-common-core/src/main/java/com/baidu/shop/status/HTTPStatus.java
@@ -0,0 +1,14 @@
+package com.baidu.shop.status;
+
+/**
+ * @ClassName HttpStatus
+ * @Description: TODO
+ * @Author yuanli
+ * @Date 2020/8/27
+ * @Version V1.0
+ **/
+public class HTTPStatus {
+    public static final int OK = 200;//成功
+
+    public static final int ERROR = 500;//失败
+}
